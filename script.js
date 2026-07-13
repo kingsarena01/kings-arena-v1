821568164e210019601e72bee5f1481a976fb2dd
kingsarena01 <thuraesport01@gmail.com>
2026-07-14 01:24:27 +0630

    Refactor player search functionality in script.js

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
 // King's Arena V1
-console.log("King's Arena V1 Loaded!");
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("Website Ready!");
-});
-const searchInput = document.getElementById("playerSearch");
 
-if (searchInput) {
-  searchInput.addEventListener("keyup", function () {
-    const keyword = this.value.toLowerCase();
-    const players = document.querySelectorAll(".player-card");
+  const searchInput = document.getElementById("playerSearch");
 
-    players.forEach(function(card) {
-      const name = card.dataset.player.toLowerCase();
+  if (searchInput) {
 
-      if (name.includes(keyword)) {
-        card.style.display = "block";
-      } else {
-        card.style.display = "none";
-      }
+    searchInput.addEventListener("keyup", function () {
+
+      const keyword = this.value.toLowerCase();
+
+      const players = document.querySelectorAll(".player-card");
+
+      players.forEach(function(card) {
+
+        const name = card.dataset.player.toLowerCase();
+
+        if (name.includes(keyword)) {
+          card.style.display = "block";
+        } else {
+          card.style.display = "none";
+        }
+
+      });
+
     });
-  });
+
+  }
+
 });
