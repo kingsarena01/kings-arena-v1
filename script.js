d0c6577acacbf54e8b5e3beb0647903f95f917df
kingsarena01 <thuraesport01@gmail.com>
2026-07-14 00:58:58 +0630

    Add player search
    Add player search functionality to filter player cards based on user input.

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ console.log("King's Arena V1 Loaded!");
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Website Ready!");
 });
+const searchInput = document.getElementById("playerSearch");
+
+if (searchInput) {
+  searchInput.addEventListener("keyup", function () {
+    const keyword = this.value.toLowerCase();
+    const players = document.querySelectorAll(".player-card");
+
+    players.forEach(function(card) {
+      const name = card.dataset.player.toLowerCase();
+
+      if (name.includes(keyword)) {
+        card.style.display = "block";
+      } else {
+        card.style.display = "none";
+      }
+    });
+  });
+}
